fb9db27b54efcfc9ce4a2a2624924638903e391a
Guillermo Alcántara <32722692+Echo9k@users.noreply.github.com>
2020-07-04 20:17:44 -0600

    HardwareAssistant reorganized, active in __init__ Helper functions _hidden in Stego; PIP8 compliance changes in  GetData and Plots.

--- packages/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name='stego-pack',
-      version='0.0.2',
-      description='Helper functions to download, unzip, and augment images for steganalysis',
-      packages=['stego'],
-      zip_safe=False)
